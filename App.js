98d393269c1717cea0d3241aad564b0ef46c2bb4
alvesronald <alvesronald@outlook.com.br>
2022-07-01 17:50:39 -0300

    feat: init and HomeScreen

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text, SafeAreaView, View} from 'react-native';
+import HomeScreen from './src/screens/Home';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <HomeScreen />
+    </SafeAreaView>
+  );
+};
+
+export default App;
---
+++ src/screens/Home/styles.js
@@ -0,0 +1,50 @@
+import {StyleSheet, Dimensions} from 'react-native';
+
+const styles = StyleSheet.create({
+  image: {
+    width: '100%',
+    height: 500,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 80,
+    color: 'white',
+    fontWeight: 'bold',
+    width: '50%',
+    marginLeft: 25,
+  },
+  button: {
+    backgroundColor: '#FFF',
+    width: 200,
+    height: 40,
+    marginLeft: 25,
+    marginTop: 25,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+  },
+  buttonText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  searchButton: {
+    backgroundColor: '#FFF',
+    width: Dimensions.get('screen').width - 20,
+    marginHorizontal: 10,
+    height: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 30,
+    flexDirection: 'row',
+    position: 'absolute',
+    top: 20,
+    zIndex: 10,
+  },
+  searchButtonText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
+
+export default styles;
